ace095115302fe6cff140510a93e9062769913bd
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-11-21 14:55:33 -0300

    JUCAS

--- JavaScript/registerValidation.js
+++ JavaScript/registerValidation.js
@@ -1,25 +1,23 @@
 var data = [];
 var storage = window.localStorage;
-const user = JSON.parse(localStorage.getItem('data'));
+const user = JSON.parse(storage.getItem('user'));
 
 $(document).ready(function () {
   clickEvent();
 });
 
-
 function clickEvent() {
-  $("#register").click(function() {
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var password = $("#password").val();
-    var confirm = $("#confirm").val();
+  $('#register').click(function () {
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var confirm = $('#confirm').val();
 
-
-    if (name == "" || email == "" || password == "" || confirm == "") {
-      alert("Preencha os campos obigatórios! Tente novamente!");
+    if (name == '' || email == '' || password == '' || confirm == '') {
+      alert('Preencha os campos obigatórios! Tente novamente!');
     } else {
       if (password == confirm) {
-        var aux = []
+        var aux = [];
 
         aux.push(name);
         aux.push(email);
@@ -29,20 +27,21 @@ function clickEvent() {
         data.push(aux);
 
         if (user == null || user.length == 0) {
-          storage.setItem("user", JSON.stringify(data));
+          storage.setItem('user', JSON.stringify(data));
         } else {
-          for (c = 0; c < user.length + 1; c++) {
+          console.log('gay');
+          for (c = 0; c < user.length; c++) {
             data.push(user[c]);
-            storage.setItem("user", JSON.stringify(data));
+            storage.setItem('user', JSON.stringify(data));
           }
         }
 
-        $("input").val("");
-        window.location.href = "../HTML/login.html";
+        $('input').val('');
+        window.location.href = '../HTML/login.html';
       } else {
-        alert("Senhas devem coincidir! Tente novamente!");
-        $("#password").val("");
-        $("#confirm").val("");
+        alert('Senhas devem coincidir! Tente novamente!');
+        $('#password').val('');
+        $('#confirm').val('');
       }
     }
   });
